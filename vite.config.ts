8fccaebd6a83778d536543021a0fffe2fa17b2d5
charly757 <chaquegm@gmail.com>
2025-12-30 19:08:30 -0300

    inicio para web pages  3

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/costos-app/", // <- clave para GitHub Pages
+  base: "./", // <- clave para GitHub Pages
 });
